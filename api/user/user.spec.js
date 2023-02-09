9e64b2a93be7786a57fc5c2754c8003c52a89c7b
jiUmin <wjdwkjh94@gmail.com>
2023-02-10 02:15:35 +0900

    sequelize설치, sqlite3  설치

--- api/user/user.spec.js
+++ api/user/user.spec.js
@@ -3,112 +3,112 @@ const app = require("../../");
 const request = require("supertest");
 const should = require("should");
 
-describe("GET /users는", () => {
-  describe("성공시", () => {
-    it(" ", (done) => {
-      request(app)
-        .get("/users")
-        .end((err, res) => {
-          res.body.should.be.instanceOf(Array);
-          done(); //비동기
-        });
-    });
+// describe("GET /users는", () => {
+//   describe("성공시", () => {
+//     it(" ", (done) => {
+//       request(app)
+//         .get("/users")
+//         .end((err, res) => {
+//           res.body.should.be.instanceOf(Array);
+//           done(); //비동기
+//         });
+//     });
 
-    it("최대 limit 갯수만큼 응답한다", (done) => {
-      request(app)
-        .get("/users?limit=2")
-        .end((err, res) => {
-          res.body.should.have.lengthOf(2);
-          done();
-        });
-    });
-  });
-  describe("실패시", () => {
-    it("limit이 숫자형이 아니면 400을 응답한다.", (done) => {
-      request(app).get("/users?limit=two").expect(400).end(done);
-    });
-  });
-});
+//     it("최대 limit 갯수만큼 응답한다", (done) => {
+//       request(app)
+//         .get("/users?limit=2")
+//         .end((err, res) => {
+//           res.body.should.have.lengthOf(2);
+//           done();
+//         });
+//     });
+//   });
+//   describe("실패시", () => {
+//     it("limit이 숫자형이 아니면 400을 응답한다.", (done) => {
+//       request(app).get("/users?limit=two").expect(400).end(done);
+//     });
+//   });
+// });
 
-describe("GET /users/1는", () => {
-  describe("성공시", () => {
-    it("id가 1인 유저 객체를 반환한다.", (done) => {
-      request(app)
-        .get("/users/1")
-        .end((err, res) => {
-          res.body.should.have.property("id", 1);
-          done();
-        });
-    });
-  });
-  describe("실패시", () => {
-    it("id가 숫자가 아닐경우 400으로 응답한다.", (done) => {
-      request(app).get("/users/one").expect(400).end(done);
-    });
-    it("id로 유저를 찾을수 없을 경우 404으로 응답한다.", (done) => {
-      request(app).get("/users/999").expect(404).end(done);
-    });
-  });
-});
+// describe("GET /users/1는", () => {
+//   describe("성공시", () => {
+//     it("id가 1인 유저 객체를 반환한다.", (done) => {
+//       request(app)
+//         .get("/users/1")
+//         .end((err, res) => {
+//           res.body.should.have.property("id", 1);
+//           done();
+//         });
+//     });
+//   });
+//   describe("실패시", () => {
+//     it("id가 숫자가 아닐경우 400으로 응답한다.", (done) => {
+//       request(app).get("/users/one").expect(400).end(done);
+//     });
+//     it("id로 유저를 찾을수 없을 경우 404으로 응답한다.", (done) => {
+//       request(app).get("/users/999").expect(404).end(done);
+//     });
+//   });
+// });
 
-describe("DELETE /users/1는", () => {
-  describe("성공시", () => {
-    it("204를 응답한다.", (done) => {
-      request(app).delete("/users/1").expect(204).end(done);
-    });
-  });
-  describe("실패시", () => {
-    it("id가 숫자가 아닐경우 400으로 응답한다.", (done) => {
-      request(app).delete("/users/one").expect(400).end(done);
-    });
-  });
-});
+// describe("DELETE /users/1는", () => {
+//   describe("성공시", () => {
+//     it("204를 응답한다.", (done) => {
+//       request(app).delete("/users/1").expect(204).end(done);
+//     });
+//   });
+//   describe("실패시", () => {
+//     it("id가 숫자가 아닐경우 400으로 응답한다.", (done) => {
+//       request(app).delete("/users/one").expect(400).end(done);
+//     });
+//   });
+// });
 
-describe("POST /users는", () => {
-  describe("성공시", () => {
-    let name = "daniel",
-      body;
+// describe("POST /users는", () => {
+//   describe("성공시", () => {
+//     let name = "daniel",
+//       body;
 
-    //밑에 테스트케이스 코드가 진행되기전에 실행됨 (모카의 함수)
-    before((done) => {
-      request(app)
-        .post("/users")
-        .send({ name: name })
-        .expect(201)
-        .end((err, res) => {
-          body = res.body;
-          done();
-        });
-    });
+//     //밑에 테스트케이스 코드가 진행되기전에 실행됨 (모카의 함수)
+//     before((done) => {
+//       request(app)
+//         .post("/users")
+//         .send({ name: name })
+//         .expect(201)
+//         .end((err, res) => {
+//           body = res.body;
+//           done();
+//         });
+//     });
 
-    it("생성된 유저 객체를 반환한다.", () => {
-      body.should.have.property("id");
-    });
-    it("입력한 name을 반환한다.", () => {
-      body.should.have.property("name", name);
-    });
-  });
+//     it("생성된 유저 객체를 반환한다.", () => {
+//       body.should.have.property("id");
+//     });
+//     it("입력한 name을 반환한다.", () => {
+//       body.should.have.property("name", name);
+//     });
+//   });
 
-  describe("실패시", () => {
-    it("name파라미터 누락시 400을 반환한다.", (done) => {
-      request(app).post("/users").send({}).expect(400).end(done);
-    });
-    it("name이 중복일 경우 409을 반환한다.", (done) => {
-      request(app)
-        .post("/users")
-        .send({ name: "daniel" })
-        .expect(409)
-        .end(done);
-    });
-  });
-});
+//   describe("실패시", () => {
+//     it("name파라미터 누락시 400을 반환한다.", (done) => {
+//       request(app).post("/users").send({}).expect(400).end(done);
+//     });
+//     it("name이 중복일 경우 409을 반환한다.", (done) => {
+//       request(app)
+//         .post("/users")
+//         .send({ name: "daniel" })
+//         .expect(409)
+//         .end(done);
+//     });
+//   });
+// });
 
 describe("PUT /users/:id는", () => {
   describe("성공시", () => {
     it("변경된 name을 응답한다", (done) => {
-      const name = "chally";
+      const name = "bbb";
       request(app)
-        .put("/users/3")
+        .put("/users/2")
         .send({ name })
         .end((err, res) => {
           res.body.should.have.property("name", name);
@@ -124,11 +124,7 @@ describe("PUT /users/:id는", () => {
       request(app).put("/users/1").send({}).expect(400).end(done);
     });
     it("없는 유저일 경우 404을 응답한다", (done) => {
-      request(app)
-        .put("/users/999")
-        .send({ name: "foo" })
-        .expect(404)
-        .end(done);
+      request(app).put("/users/99").send({ name: "foo" }).expect(404).end(done);
     });
     it("이름이 중복일 경우 409을 응답한다", (done) => {
       request(app).put("/users/3").send({ name: "bek" }).expect(409).end(done);
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 var morgan = require("morgan");
 const bodyParser = require("body-parser");
-var user = require("./api/user");
+var user = require("./api/user"); //require("./api/user/index.js"); index.js 생략 가능
 
 if (process.env.NODE_ENV !== "test") {
   app.use(morgan("dev"));
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV !== "test") {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/users", user);
+app.use("/users", user); ///users로 오는 모든 api요청은 user모듈이 담당
 
 // app.listen(port, () => {
 //   console.log(`Example app listening on port ${port}`);
